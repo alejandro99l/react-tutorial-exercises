1736000227038ed2a0516321b5374567cc62c947
Alejandro <alejandrolujan1999@outlook.es>
2024-10-26 00:09:32 +0000

    Práctica React

--- .learn/resets/03-render-with-functions/app.jsx
+++ .learn/resets/03-render-with-functions/app.jsx
@@ -4,8 +4,7 @@ import ReactDOM from "react-dom"; // We use ReactDOM to render into the DOM
 
 // This function returns a string that will be rendered
 export const PrintHello = () => {
-	const mensaje = "I Love React";
-	return <h1>{mensaje}</h1>;
+	return <h1>Hello World</h1>;
 };
 
 //              what           where
